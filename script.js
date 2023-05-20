31178fe3489b941f75eb28d42a49c59fdf423982
muthee-p <pruemu9@gmail.com>
2023-05-20 17:19:16 +0300

    added time and count

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ const images = [
 let cards = [];
 let card1 = null;
 let card2 = null;
+let clickCount = 0;
+let matchCount = 0;
+let startTime = null;
+let timerInterval = null;
 
 function createImageElement(src, index) {
   const img = document.createElement('img');
@@ -43,14 +47,22 @@ function createGrid() {
   const selectedImages = images.slice(0, 8);
 
   // Duplicate each image in the selected array
-  cards = selectedImages.concat(selectedImages);
+  const duplicateImages = selectedImages.concat(selectedImages);
 
-  shuffleArray(cards);
+  shuffleArray(duplicateImages);
 
-  cards.forEach(function (image, index) {
-    gridContainer.appendChild(createImageElement(image, index));
-  });
+  // Create a 4x4 grid layout
+  const grid = document.createElement('div');
+  grid.className = 'grid';
+
+  for (let i = 0; i < 16; i++) {
+    const card = createImageElement(duplicateImages[i], i);
+    grid.appendChild(card);
+  }
+
+  gridContainer.appendChild(grid);
 }
+
 
 
 function clicked(card, index) {
@@ -67,6 +79,13 @@ function clicked(card, index) {
       index: index
     };
 
+    clickCount++;
+    document.getElementById('clickCount').textContent = clickCount;
+
+    if (clickCount === 1) {
+      startTime = new Date();
+      timerInterval = setInterval(updateTimer, 1000);
+    }
     setTimeout(checkMatch, 1000);
   }
 }
@@ -75,6 +94,16 @@ function checkMatch() {
   if (card1.element.src == card2.element.src) {
     card1 = null;
     card2 = null;
+     matchCount++;
+
+      if (matchCount === 8) {
+      clearInterval(timerInterval);
+      const endTime = new Date();
+      const gameTime = Math.floor((endTime - startTime) / 1000);
+      setTimeout(function () {
+        showPopup(gameTime, clickCount);
+      }, 500);
+    }
   } else {
     card1.element.classList.remove('show');
     card2.element.classList.remove('show');
@@ -84,12 +113,48 @@ function checkMatch() {
   }
 }
 
+function updateTimer() {
+  const currentTime = new Date();
+  const gameTime = Math.floor((currentTime - startTime) / 1000);
+  document.getElementById('timer').textContent = gameTime;
+}
+
+function showPopup(gameTime, clickCount) {
+  const popup = document.createElement('div');
+  popup.className = 'popup';
+
+  const message = document.createElement('p');
+  message.textContent = `Congratulations! You completed the game in ${gameTime} seconds with ${clickCount} moves.`;
+  popup.appendChild(message);
+
+  const playAgainButton = document.createElement('button');
+  playAgainButton.textContent = 'Play Again';
+  playAgainButton.onclick = resetGame;
+  popup.appendChild(playAgainButton);
+
+  document.body.appendChild(popup);
+}
+
+
 function resetGame() {
   card1 = null;
   card2 = null;
+  clickCount = 0;
+  matchCount = 0;
+  startTime = null;
+  clearInterval(timerInterval);
+
   const gridContainer = document.getElementById('gridContainer');
   gridContainer.innerHTML = '';
+
+  const popup = document.querySelector('.popup');
+  if (popup) {
+    popup.remove();
+  }
+
   createGrid();
+  document.getElementById('clickCount').textContent = clickCount;
+  document.getElementById('timer').textContent = '0';
 }
 
 // Initialize the game
